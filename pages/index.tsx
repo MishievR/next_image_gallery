2bbb0ce8e043228b4147e1be3ab4d5fd1396b4fa
MishievR <mr.mishiev@gmail.com>
2022-03-31 16:09:58 +0300

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ import { createClient } from '@supabase/supabase-js';
 
 export async function getStaticProps() {
   const supabaseAdmin = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL || ' ',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ' '
   )
 
   const { data } = await supabaseAdmin
